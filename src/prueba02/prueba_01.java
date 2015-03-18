572030b4f04f7b56e0912294d3c8dc15115c9ee8
alumno04 <alumno04@TPP-LAB04-15.TPP.UPEU.EDU.PE>
2015-03-18 10:34:27 -0500

    se creo la segunda clase del proyecto

---
+++ src/prueba02/prueba_01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba02;
+
+/**
+ *
+ * @author alumno04
+ */
+public class prueba_01 {
+    int a=1;
+    
+    for(a=1)
+    {
+    
+    }
+}
